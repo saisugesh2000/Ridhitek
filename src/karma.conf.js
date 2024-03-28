e0d78cd6fdaed844779b003ae51372f88b868b8a
sai sugesh kumar <92208947+saisugesh2000@users.noreply.github.com>
2024-03-28 15:59:16 +0530

    Create karma.conf.js
    Record testcase execution Data

---
+++ src/karma.conf.js
@@ -0,0 +1,17 @@
+Module.exports = function(config){
+config.set({
+... 
+...
+ plugins: [
+   require('karma-jasmine'),
+   require('karma-chrome-launcher'),
+   require('karma-jasmine-html-reporter'),
+   require('karma-coverage-istanbul-reporter'),
+   require('@angular/cli/plugins/karma'),
+   require('karma-jasmine-diff-reporter'),
+   require('karma-structured-json-reporter')
+ ],
+ browsers: ['Chrome'],
+…
+...
+}
